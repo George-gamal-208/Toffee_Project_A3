1409a07cd606d19dbb3c57f0ac309c0d94d14a32
george <georgeayman699@gmail.com>
2023-05-06 21:28:36 +0300

    create classes

---
+++ Toffee.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class User{
+
+}
+class Admin{
+
+}
+class Customer{
+
+}
+class Shopping_Cart{
+    
+}
+public class Toffee{
+    public static void main(String[] args){
+        //System.out.println("A");
+        Scanner u = new Scanner(System.in);
+        int g=u.nextInt();
+        System.out.println(g);
+    }
+}
